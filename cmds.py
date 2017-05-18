92bc90137cba83236cb22f2c450fd2cf60f574a7
harshil2105 <harsaxena@gmail.com>
2017-05-18 20:11:34 +0200

    Update cmds.py

--- cmds.py
+++ cmds.py
@@ -8,3 +8,10 @@ class cmd:
         proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
         stdout_value = proc.stdout.read() + proc.stderr.read()
         return stdout_value
+class getidletime:
+    def __init__(self):
+        self.command = 'getidletime'
+        self.description = 'printing user idle time in seconds\n'
+        self.args = False
+    def main(self):
+        return (win32api.GetTickCount() - win32api.GetLastInputInfo()) / 1000.0
