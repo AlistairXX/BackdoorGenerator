54470ae527684f22773445f4b73b3c2a1e11c4c4
harshil2105 <harsaxena@gmail.com>
2017-05-19 10:51:48 +0200

    Create sock.py

---
+++ sock.py
@@ -0,0 +1,7 @@
+import socket
+class net:
+    def __init__(self):
+         self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    def news(self):
+         self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM
+sock = net()
