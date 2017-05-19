dd391933a108e4c97b51f2f6d7708dcbb77a3e20
harshil2105 <harsaxena@gmail.com>
2017-05-19 18:38:04 +0200

    Update cmds.py

--- cmds.py
+++ cmds.py
@@ -1,4 +1,4 @@
-import subprocess,win32api,sock
+import subprocess,win32api,ctypes,getpass,sock
 class cmd:
     def __init__(self):
         self.command = 'cmd'
@@ -15,3 +15,19 @@ class getidletime:
         self.args = False
     def main(self):
         return str((win32api.GetTickCount() - win32api.GetLastInputInfo()) / 1000.0)+'\n'
+class getuser:
+    def __init__(self):
+        self.command = 'getusername'
+        self.description = 'printing the username of the currently logged in user'
+        self.args = False
+    def main(self):
+        return getpass.getuser()+'\n'
+class messagebox:
+    def __init__(self):
+        self.command = 'messagebox'
+        self.description = 'generating a custom messagebox with: messagebox <message> <window title>'
+    def main(self,arg):
+        Box = ctypes.windll.user32.MessageBoxA
+        sock.sock.s.send("Box was opened\n")
+        Box(None,args.split(" ")[0],args.split(" ")[1], 0)
+        return "Message box was closed\n"
