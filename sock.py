b99908b0732d194abba90a9c343f7028b090c72f
harshil2105 <harsaxena@gmail.com>
2017-05-19 11:04:38 +0200

    Update sock.py

--- sock.py
+++ sock.py
@@ -3,5 +3,5 @@ class net:
     def __init__(self):
          self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     def news(self):
-         self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM
+         self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 sock = net()
